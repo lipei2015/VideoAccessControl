a27838760f75202397139acb1f6b047282b6a476
465813771@qq.com <lplp19890817>
2019-08-06 18:39:00 +0800

    2019年8月6日18:07:08

---
+++ app/src/main/java/com/ybkj/videoaccess/mvp/data/bean/DataInfo.java
@@ -0,0 +1,13 @@
+package com.ybkj.videoaccess.mvp.data.bean;
+
+public class DataInfo {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
